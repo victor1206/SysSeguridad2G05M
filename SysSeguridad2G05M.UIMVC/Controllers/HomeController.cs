20dada6bd133fd58f613c2d193a2ed2f0766c514
victor1206 <victor_rivas89@hotmail.com>
2026-03-16 10:08:25 -0600

    Creacion de proyecto base

---
+++ SysSeguridad2G05M.UIMVC/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using SysSeguridad2G05M.UIMVC.Models;
+using System.Diagnostics;
+
+namespace SysSeguridad2G05M.UIMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
