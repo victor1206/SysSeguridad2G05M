79bfdb73f9a72925f404c0552b5f138c1ba84917
victor1206 <victor_rivas89@hotmail.com>
2026-03-18 11:20:36 -0600

    Se agrego la clase usuario dal del proyecto

---
+++ SysSeguridad2G05M.DAL/UsuarioDAL.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SysSeguridad2G05M.EN;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SysSeguridad2G05M.DAL
+{
+    public class UsuarioDAL
+    {
+        private static void EncriptarMD5(Usuario pUsuario)
+        {
+            using (var md5 = MD5.Create())
+            {
+                var result = md5.ComputeHash(Encoding.UTF8.GetBytes(
+                    pUsuario.Password));
+                var strEncriptar = "";
+                for (int i = 0; i < result.Length; i++)
+                    strEncriptar += result[i].ToString("x2").ToLower();
+                pUsuario.Password = strEncriptar;
+            }
+        }
+
+        private static async Task<bool> ExisteLogin(Usuario pUsuario,
+            DBContexto pDContexto)
+        { 
+            bool result = false;
+            var loginUsuarioExiste = await pDContexto.Usuario.FirstOrDefaultAsync(
+                a => a.Login == pUsuario.Login && a.Id != pUsuario.Id);
+            if (loginUsuarioExiste != null && loginUsuarioExiste.Id > 0 &&
+                loginUsuarioExiste.Login == pUsuario.Login)
+                result = true;
+            return result;
+        }
+
+        #region "CRUD"
+        public static async Task<int> GuardarAsync(Usuario pUsuario)
+        { 
+            int result = 0;
+            try
+            {
+                using (var dbContexto = new DBContexto())
+                {
+                    bool existeLogin = await ExisteLogin(pUsuario, dbContexto);
+                    if (existeLogin == false)
+                    {
+                        EncriptarMD5(pUsuario);
+                        dbContexto.Add(pUsuario);
+                        result = await dbContexto.SaveChangesAsync();
+                    }
+                    else
+                    {
+                        throw new Exception("Login ya existe");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                result = 0;
+                throw new Exception(ex.Message);
+            }
+            return result;
+        }
+
+        public static async Task<int> ModificarAsync(Usuario pUsuario)
+        {
+            int result = 0;
+            try
+            {
+                using (var dbContexto = new DBContexto())
+                {
+                    bool existeLogin = await ExisteLogin(pUsuario, dbContexto);
+                    if (existeLogin == false)
+                    {
+                        var usuario = await dbContexto.Usuario.FirstOrDefaultAsync(
+                            d => d.Id == pUsuario.Id);
+                        usuario.IdRol = pUsuario.IdRol;
+                        usuario.Nombre = pUsuario.Nombre;
+                        usuario.Apellido = pUsuario.Apellido;
+                        usuario.Login = pUsuario.Login;
+                        usuario.Estatus = pUsuario.Estatus;
+                        dbContexto.Update(usuario);
+                        result = await dbContexto.SaveChangesAsync();
+                    }
+                    else
+                        throw new Exception("Login ya existe");
+                }
+            }
+            catch (Exception ex)
+            {
+                result = 0;
+                throw new Exception(ex.Message);
+            }
+            return result;
+        }
+        #endregion
+
+
+    }
+}
