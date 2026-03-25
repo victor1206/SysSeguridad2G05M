986c88bbfec2e3e998c46b9cd20f6e1e25f5f747
victor1206 <victor_rivas89@hotmail.com>
2026-03-25 11:11:55 -0600

    Se agrega cambio de usuario

---
+++ SysSeguridad2G05M.BL/UsuarioBL.cs
@@ -0,0 +1,48 @@
+﻿using SysSeguridad2G05M.DAL;
+using SysSeguridad2G05M.EN;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SysSeguridad2G05M.BL
+{
+    public class UsuarioBL
+    {
+        public async Task<int> CrearAsync(Usuario pUsuario)
+        {
+            return await UsuarioDAL.GuardarAsync(pUsuario);
+        }
+
+        public async Task<int> ModificarAsync(Usuario pUsuario)
+        {
+            return await UsuarioDAL.ModificarAsync(pUsuario);
+        }
+
+        public async Task<int> EliminarAsync(Usuario pUsuario)
+        {
+            return await UsuarioDAL.EliminarAsync(pUsuario);
+        }
+
+        public async Task<Usuario> ObtenerPorIdAsync(Usuario pUsuario)
+        {
+            return await UsuarioDAL.ObtenerPorId(pUsuario);
+        }
+
+        public async Task<List<Usuario>> ObtenerTodosAsync()
+        {
+            return await UsuarioDAL.ObtenerTodosAsync();
+        }
+
+        public async Task<List<Usuario>> BuscarAsync(Usuario pUsuario)
+        {
+            return await UsuarioDAL.BuscarAsync(pUsuario);
+        }
+
+        public async Task<List<Usuario>> BuscarIncluirRolesAsync(Usuario pUsuario)
+        {
+            return await UsuarioDAL.BuscarIncluirRolesAsync(pUsuario);
+        }
+    }
+}
--- SysSeguridad2G05M.UIMVC/Controllers/UsuarioController.cs
+++ SysSeguridad2G05M.UIMVC/Controllers/UsuarioController.cs
@@ -1,67 +1,69 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-using SysSeguridad2G05M.BL;
 using SysSeguridad2G05M.EN;
+using SysSeguridad2G05M.BL;
 
 namespace SysSeguridad2G05M.UIMVC.Controllers
 {
-    public class RolController : Controller
+    public class UsuarioController : Controller
     {
-        RolBL rolbl = new RolBL();
-        // GET: RolController
-        public async Task<IActionResult> Index(Rol pRol = null)
+        UsuarioBL usuariobl = new UsuarioBL();
+        RolBL rolBL = new RolBL();
+        // GET: UsuarioController
+        public  async Task<IActionResult> Index(Usuario pUsuario = null)
         {
-            if(pRol == null)
-                pRol = new Rol();
-            if(pRol.Top_Aux == 0)
-                pRol.Top_Aux = 10;
+            if (pUsuario == null)
+                pUsuario = new Usuario();
+            if (pUsuario.Top_Aux == 0)
+                pUsuario.Top_Aux = 10;
             else
-                if(pRol.Top_Aux == -1)
-                    pRol.Top_Aux = 0;
+                if (pUsuario.Top_Aux == -1)
+                    pUsuario.Top_Aux = 0;
 
-            var roles = await rolbl.BuscarAsync(pRol);
-            ViewBag.Top = pRol.Top_Aux;
-            return View(roles);
+            var taksBuscar = await usuariobl.BuscarIncluirRolesAsync(pUsuario);
+            var taskObtenerRoles = rolBL.ObtenerTodosAsync();
+
+            ViewBag.Top = pUsuario.Top_Aux;
+            ViewBag.Roles = await taskObtenerRoles;
+
+            return View(taksBuscar);
         }
 
-        // GET: RolController/Details/5
+        // GET: UsuarioController/Details/5
         public ActionResult Details(int id)
         {
             return View();
         }
 
-        // GET: RolController/Create
-        public IActionResult Create()
+        // GET: UsuarioController/Create
+        public ActionResult Create()
         {
-            ViewBag.Error = "";
             return View();
         }
 
-        // POST: RolController/Create
+        // POST: UsuarioController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(Rol pRol)
+        public ActionResult Create(IFormCollection collection)
         {
             try
             {
-                int result = await rolbl.CrearAsync(pRol);
                 return RedirectToAction(nameof(Index));
             }
-            catch(Exception ex)
+            catch
             {
-                ViewBag.Error = ex.Message;
-                return View(pRol);
+                return View();
             }
         }
 
-        // GET: RolController/Edit/5
+        // GET: UsuarioController/Edit/5
         public ActionResult Edit(int id)
         {
             return View();
         }
 
-        // POST: RolController/Edit/5
+        // POST: UsuarioController/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Edit(int id, IFormCollection collection)
@@ -76,13 +78,13 @@ namespace SysSeguridad2G05M.UIMVC.Controllers
             }
         }
 
-        // GET: RolController/Delete/5
+        // GET: UsuarioController/Delete/5
         public ActionResult Delete(int id)
         {
             return View();
         }
 
-        // POST: RolController/Delete/5
+        // POST: UsuarioController/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Delete(int id, IFormCollection collection)
